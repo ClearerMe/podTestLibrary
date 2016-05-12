046a030cf8267669227553c69b4542d31d174976
huxiao <huxiao@geek-zoo.com>
2016-05-12 17:05:19 +0800

    Initial commit

---
+++ Example/podTestLibrary/PTLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PTLAppDelegate.h
+//  podTestLibrary
+//
+//  Created by huxiao on 05/12/2016.
+//  Copyright (c) 2016 huxiao. All rights reserved.
+//
+
+@import UIKit;
+
+@interface PTLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/podTestLibrary/PTLViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTLViewController.h
+//  podTestLibrary
+//
+//  Created by huxiao on 05/12/2016.
+//  Copyright (c) 2016 huxiao. All rights reserved.
+//
+
+@import UIKit;
+
+@interface PTLViewController : UIViewController
+
+@end
